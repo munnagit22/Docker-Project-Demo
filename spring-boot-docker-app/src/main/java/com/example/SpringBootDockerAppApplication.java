dd711ec6b585604e0007862dc7d0d3e20b3cb7e6
MUNNA IT <munna.mca07@gmail.com>
2022-10-02 18:56:20 +0530

    main class modify

--- spring-boot-docker-app/src/main/java/com/example/SpringBootDockerAppApplication.java
+++ spring-boot-docker-app/src/main/java/com/example/SpringBootDockerAppApplication.java
@@ -8,6 +8,7 @@ public class SpringBootDockerAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDockerAppApplication.class, args);
+		System.out.println("Application running.........!");
 	}
 
 }
